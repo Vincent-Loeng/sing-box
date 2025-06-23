dac20b4a8e0b1a83fd37e017e1d7efbefa1eb33d
Vincent Loeng <173226116+Vincent-Loeng@users.noreply.github.com.>
2025-06-23 06:31:13 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+## apply patches
 cd ../sing-tun
 # freebsd
 cp monitor_darwin.go monitor_freebsd.go
@@ -24,21 +25,28 @@ sed -i 's|DarwinEndpoint|FreeBSDEndpoint|g;s|darwin|freebsd|g' tun_freebsd_gviso
 #sed -i 's|DarwinEndpoint|DragonFlyEndpoint|g;s|darwin|dragonfly|g' tun_dragonfly_gvisor.go
 git diff HEAD > ../mihomo/sing-tun.patch
 
+
+## build
+# sing-box
 os='freebsd'
 arch='amd64'
 sb_version='1.12.0-beta-vincent'
 tags='with_gvisor,with_quic,with_dhcp,with_wireguard,with_utls,with_acme,with_clash_api,with_tailscale'
-dragonfly_tags='with_gvisor,with_quic,with_dhcp,with_utls,with_acme,with_clash_api'
+no_wg_tags='with_gvisor,with_quic,with_dhcp,with_utls,with_acme,with_clash_api'
+
 cd ../sing-box-official
 git diff HEAD > ../sing-box/sing-box.patch
-if [ ${os} == "dragonfly" ]; then
-  GOOS=${os} GOARCH=${arch} CGO_ENABLED=0 go build -v -trimpath -ldflags "-X 'github.com/sagernet/sing-box/constant.Version=${sb_version}' -s -w -buildid=" -tags ${dragonfly_tags} ./cmd/sing-box
+if [ ${os} == "dragonfly" -o ${os} == "netbsd" ]; then
+  GOOS=${os} GOARCH=${arch} CGO_ENABLED=0 go build -v -trimpath -ldflags "-X 'github.com/sagernet/sing-box/constant.Version=${sb_version}' -s -w -buildid=" -tags ${no_wg_tags} ./cmd/sing-box
 else
   GOOS=${os} GOARCH=${arch} CGO_ENABLED=0 go build -v -trimpath -ldflags "-X 'github.com/sagernet/sing-box/constant.Version=${sb_version}' -s -w -buildid=" -tags ${tags} ./cmd/sing-box
 fi
 #rm sing-box
-
+# mihomo
 cd ../mihomo-official
 git diff HEAD > ../mihomo/mihomo.patch
+if [ ${os} == "dragonfly" -o ${os} == "netbsd" ]; then
+  exit 1
+fi
 GOOS=${os} GOARCH=${arch} CGO_ENABLED=0 go build -v -tags with_gvisor -trimpath -ldflags '-X "github.com/metacubex/mihomo/constant.Version=Alpha-vincent" -X "github.com/metacubex/mihomo/constant.BuildTime=none" -w -s -buildid=' -o mihomo
 #rm mihomo
